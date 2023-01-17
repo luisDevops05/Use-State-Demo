404507427df2e9b63abfbaa6a70de08d3d71b495
JlSanchez05 <luisdevops0@gmail.com>
2023-01-16 22:39:54 -0500

    added files to work, and started the main idea

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+
+    const [count,setCount] = useState(1)
+
+        const handleClick = () =>{
+        setCount(count+1)
+        console.log(count)
+            const promt = document.querySelector('.promt')
+
+        if(count ===1){
+            console.log('hello')
+            promt.style.display='block'
+        }
+
+     
 }
 
-export default App;
+
+
+    return(
+        <div>
+            <section className="headingSection">
+                <div className="headingContainer">
+                <div className="heading">
+                    <h1>Welcome</h1>
+                </div>
+             </div>
+        </section>
+
+        <section className='btnSection'>
+            <div className="btnContainer">
+                <div className="btnC">
+                    <button id="btn" onClick={handleClick}>Click Here</button>
+                </div>
+                <div className='promt'><p>Welcome Im a progammed AI to help you with what you need. How can I Help you today?</p> </div>
+            </div>
+        </section>
+
+
+
+
+
+        </div>
+        
+       
+     
+    )
+}
+
+
+
+export default App
